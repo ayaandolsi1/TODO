f18b9a6313dfde4ef8dae2ff88b59663762d6116
ayaandolsi1 <161944149+ayaandolsi1@users.noreply.github.com>
2024-04-03 04:10:17 +0200

    first commit

---
+++ OneDrive/Desktop/ajoutempl/ajoutpersonnel/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddEmployer from './AddEmployer'; 
+function App() {
+  return (
+    <div>
+      
+      <AddEmployer /> 
+    </div>
+  );
+}
+
+export default App;
